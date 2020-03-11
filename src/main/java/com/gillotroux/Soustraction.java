be2fdefc4d5e21acd245fa454b31ffca1cb389e1
Lockolocko <cmoirafi@gmail.com>
2020-03-11 12:23:09 +0100

    Classe soustraction

--- src/main/java/com/gillotroux/Soustraction.java
+++ src/main/java/com/gillotroux/Soustraction.java
@@ -1,6 +1,6 @@
 package com.gillotroux;
 
-public class Addition implements SpecificCommand {
+public class Soustraction implements SpecificCommand {
 
     public void apply() {
         // TODO Auto-generated method stub
