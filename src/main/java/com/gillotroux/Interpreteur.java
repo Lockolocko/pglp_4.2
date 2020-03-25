ab6b9b6091d3722a960ac0f7e25e1d564552fff4
Lockolocko <cmoirafi@gmail.com>
2020-03-25 22:44:41 +0100

    Undo modification

--- src/main/java/com/gillotroux/Interpreteur.java
+++ src/main/java/com/gillotroux/Interpreteur.java
@@ -17,21 +17,33 @@ public class Interpreteur {
      * Commandes quit et undo.
      */
     private Map<String,GeneriqueCommand> commands;
-
+    /**
+     * Constructeur.
+     */
     private Interpreteur () {
         this.commands=new HashMap<String,GeneriqueCommand>();
     }
-    
+    /**
+     * Créer la commande name qui effectue command.
+     * @param name
+     * @param command
+     */
     void addCommand(String name,GeneriqueCommand command) {
         this.commands.put(name, command);
     }
- 
+    /**
+     * Execute la commande name.
+     * @param name
+     */
     void executeCommand(String name) {
         if(this.commands.containsKey(name)) {
             this.commands.get(name).apply();
         }
     }
-    
+    /**
+     * Constructeur public avec les fonctions prédéfinis.
+     * @return
+     */
     static Interpreteur init() {
         Interpreteur interpreteur = new Interpreteur();
         GeneriqueCommand quit = new Quit();
--- src/main/java/com/gillotroux/Undo.java
+++ src/main/java/com/gillotroux/Undo.java
@@ -2,16 +2,31 @@
  * Package de Gillot-Roux.
  */
 package com.gillotroux;
+
+import java.util.Stack;
+
 /**
  * Undo.
  * @author Lockolocko
  *
  */
 public class Undo implements GeneriqueCommand {
-
+    private Stack<Stack<String>> historiquePile;
+    
+    public Undo() {
+        historiquePile =new Stack<Stack<String>>();
+    }
+    
+    public void add (Stack<String> pile) {
+        historiquePile.push(pile);
+    }
+    
+    public Stack<String> getPile() {
+        return historiquePile.lastElement();
+    }
+    
     public void apply() {
-        // TODO Auto-generated method stub
-        
+        historiquePile.pop();
     }
 
 }
